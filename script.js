7f3f20f2d7ab8600b279ae5a4cb313e205e45275
estelle-gf <estellegfont@gmail.com>
2025-06-05 11:04:59 +0100

    added emoji confetti and linked pluggin

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-function createConfetti() {
-  jsConfetti.addConfetti({
-    emojis: [fruitSelect.value],
-    confettiNumber: 100,
-    emojiSize: 50
-  });
-}
+const canvas = document.getElementById('custom_canvas');
+const jsConfetti = new JSConfetti({ canvas });
 
+function addConfetti() {
+  const fruit = document.getElementById("fruit").value;
+  const fruitEmojis = {
+      greenApple: ['🍏','💚'],
+      redApple: ['🍎','♥️'],
+      banana: ['🍌','✨'],
+      cherry: ['🍒','❤️'],
+      grapes: ['🍇','💜'],
+      watermelon: ['🍉','💧'],
+      lemon: ['🍋','💛'],
+      lime: ['🍋‍🟩','🟢'],
+      honeydew: ['🍈','🍯'],
+      mango: ['🥭','🌴'],
+      blueberry: ['🫐','💙'],
+      orange: ['🍊','🟠'],
+      peach: ['🍑','🧡'],
+      strawberry: ['🍓','🫙'],
+      kiwi: ['🥝','🟢'],
+      pear: ['🍐','🌱'],
+      tomato: ['🍅','🥫'],
+      pineapple: ['🍍','🍹'],
+    };
 
+    const emojis = fruitEmojis[fruit] || ['🎉']; // Default fallback
+    jsConfetti.addConfetti({
+      emojis: emojis
+    }); }
 let fruitSelect = document.querySelector("#fruit");
-fruitSelect.addEventListener("change", createConfetti);
+fruitSelect.addEventListener("change", addConfetti);
